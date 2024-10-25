b9ab934c7082582569da285a9251d4c3cc55a5ac
Erlangga-myname <erlanggasatriacakrabirawa@gmail.com>
2024-10-25 08:54:35 +0700

    flutter_app1.4

--- main.dart
+++ main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',//diberi judul Namer App
         theme: ThemeData(//data tema aplikasi, diberi warna deepOrange
           useMaterial3: true,//versi materialUI yang dipakai versi 3
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),//nama halaman "myHomePage" pada "myAppState"
       ),
@@ -38,27 +38,64 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
 }
+var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 // ...
+  
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;               
+    var pair = appState.current; 
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),                
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair), 
+             SizedBox(height: 10),               
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -84,7 +121,11 @@ final style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-       child: Text(pair.asLowerCase, style: style),
+       child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+      ),
       ),
     );
   }
